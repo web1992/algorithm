c57345117b7e0a0662d1baed3d98fb98ba2e4361
web1992 <554933654@qq.com>
2022-07-04 10:04:11 +0800

    add [整数拆分.java]

---
+++ src/main/java/cn/web1992/algorithm/leetcode/整数拆分.java
@@ -0,0 +1,29 @@
+package cn.web1992.algorithm.leetcode;
+
+/**
+ * @link {https://leetcode.cn/problems/integer-break/}
+ * @link {https://programmercarl.com/0343.%E6%95%B4%E6%95%B0%E6%8B%86%E5%88%86.html#}
+ */
+public class 整数拆分 {
+
+    public static void main(String[] args) {
+        int ans = new Solution().integerBreak(4);
+        System.out.println(ans);
+    }
+
+    static class Solution {
+        public int integerBreak(int n) {
+
+            int[] dp = new int[n + 1];
+            dp[2] = 1;
+
+            for (int i = 3; i <= n; i++) {
+                for (int j = 1; j <= i - j; j++) {
+                    dp[i] = Math.max(dp[i], Math.max(dp[i - j] * j, j * (i - j)));
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
