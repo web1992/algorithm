1e689f6ddafb6dd770cd3e0ba40c32a9197b9c14
web1992 <554933654@qq.com>
2022-07-07 09:05:59 +0800

    add [最后一块石头的重量II.java]

---
+++ src/main/java/cn/web1992/algorithm/leetcode/test/最后一块石头的重量II.java
@@ -0,0 +1,23 @@
+package cn.web1992.algorithm.leetcode.test;
+
+/**
+ * @link {https://leetcode.cn/problems/last-stone-weight-ii/}
+ * @link {https://programmercarl.com/1049.%E6%9C%80%E5%90%8E%E4%B8%80%E5%9D%97%E7%9F%B3%E5%A4%B4%E7%9A%84%E9%87%8D%E9%87%8FII.html#_1049-%E6%9C%80%E5%90%8E%E4%B8%80%E5%9D%97%E7%9F%B3%E5%A4%B4%E7%9A%84%E9%87%8D%E9%87%8F-ii}
+ */
+public class 最后一块石头的重量II {
+    public static void main(String[] args) {
+
+        int[] stones = new int[]{2, 7, 4, 1, 8, 1};
+        int ans = new Solution().lastStoneWeightII(stones);
+        System.out.println("ans" + ans);
+
+    }
+
+    static class Solution {
+
+        public int lastStoneWeightII(int[] stones) {
+
+            return 0;
+        }
+    }
+}
