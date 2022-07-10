89f8074abddf6a6b744c0ab40630e48482fc16b8
web1992 <554933654@qq.com>
2022-07-10 23:34:48 +0800

    add [回文子串]

---
+++ src/main/java/cn/web1992/algorithm/leetcode/test/回文子串.java
@@ -0,0 +1,27 @@
+package cn.web1992.algorithm.leetcode.test;
+
+/**
+ * @author web1992
+ * @date 2022/7/9  23:30
+ * @link {https://leetcode.cn/problems/palindromic-substrings/} 题目
+ * @link {https://leetcode.cn/problems/palindromic-substrings/solution/by-xi-huan-an-xian-zi-zai-0rye/} 暴力方法
+ * @link {https://programmercarl.com/0647.%E5%9B%9E%E6%96%87%E5%AD%90%E4%B8%B2.html#_647-%E5%9B%9E%E6%96%87%E5%AD%90%E4%B8%B2}
+ */
+public class 回文子串 {
+
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+
+        // a b c
+        // i
+        //   j
+        public int countSubstrings(String s) {
+
+            return 0;
+        }
+
+    }
+}
