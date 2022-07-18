c5b06796fa2c4ba26cd1f9db3d4120d1a0966c9a
web1992 <554933654@qq.com>
2022-07-18 17:01:54 +0800

    add [注释]

--- src/main/java/cn/web1992/algorithm/leetcode/找到字符串中所有字母异位词.java
+++ src/main/java/cn/web1992/algorithm/leetcode/找到字符串中所有字母异位词.java
@@ -30,6 +30,7 @@ public class 找到字符串中所有字母异位词 {
      */
     static class Solution {
 
+        // 滑动窗口
         public List<Integer> findAnagrams(String s, String p) {
 
             List<Integer> ans = new ArrayList<>();
