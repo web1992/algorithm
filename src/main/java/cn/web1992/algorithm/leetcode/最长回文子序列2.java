cb75af32a3e675f30ff601640deb5362a40c0f15
web1992 <554933654@qq.com>
2022-08-16 18:01:27 +0800

    add [最长回文子序列.java]

--- src/main/java/cn/web1992/algorithm/leetcode/最长回文子序列2.java
+++ src/main/java/cn/web1992/algorithm/leetcode/最长回文子序列2.java
@@ -4,10 +4,10 @@ package cn.web1992.algorithm.leetcode;
  * @link {https://leetcode.cn/problems/longest-palindromic-subsequence/}
  * @link {https://programmercarl.com/0516.%E6%9C%80%E9%95%BF%E5%9B%9E%E6%96%87%E5%AD%90%E5%BA%8F%E5%88%97.html#_516-%E6%9C%80%E9%95%BF%E5%9B%9E%E6%96%87%E5%AD%90%E5%BA%8F%E5%88%97}
  */
-public class 最长回文子序列 {
+public class 最长回文子序列2 {
 
     public static void main(String[] args) {
-        String str = "bbbab";
+        String str = "bbbab";// -> bbbb(不是连续的), cbbd- > bb
         int ans = new Solution().longestPalindromeSubseq(str);
         System.out.println(ans);
     }
@@ -15,24 +15,28 @@ public class 最长回文子序列 {
     static class Solution {
 
         // 最长回文子序列
+        // 简化 dp 数组
         public int longestPalindromeSubseq(String s) {
 
             int len = s.length();
+            int maxLen = 1;
 
             int[][] dp = new int[len][len];
-
             for (int i = len - 1; i >= 0; i--) {
-                dp[i][i] = 1;// 初始化
-                for (int j = i + 1; j < len; j++) {
+                dp[i][i] = 1;
+                for (int j = i; j < len; j++) {
+                    System.out.println(i + "-" + j);
                     if (s.charAt(i) == s.charAt(j)) {
-                        dp[i][j] = dp[i + 1][j - 1] + 2;
+                        maxLen = (dp[i + 1][j - 1] + 2);
+                        dp[i][j] = maxLen;
                     } else {
-                        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                        maxLen += Math.max(dp[i + 1][j], dp[i][j - 1]);
+                        dp[i][j] = maxLen;
                     }
                 }
             }
 
-            return dp[0][len - 1];
+            return maxLen;
         }
     }
 }
