b77b5e02ebc19eafab55a706259bd764748b5a4e
web1992 <554933654@qq.com>
2022-08-17 17:51:51 +0800

    add [不同的子序列.java]

---
+++ src/main/java/cn/web1992/algorithm/leetcode/不同的子序列.java
@@ -0,0 +1,31 @@
+package cn.web1992.algorithm.leetcode;
+
+
+/**
+ * @link {https://leetcode.cn/problems/distinct-subsequences/}
+ * @link {https://programmercarl.com/0115.%E4%B8%8D%E5%90%8C%E7%9A%84%E5%AD%90%E5%BA%8F%E5%88%97.html#_115-%E4%B8%8D%E5%90%8C%E7%9A%84%E5%AD%90%E5%BA%8F%E5%88%97}
+ */
+public class 不同的子序列 {
+
+    public static void main(String[] args) {
+
+        String s = "rabbbit", t = "rabbit";
+        int ans = new Solution().numDistinct(s, t);
+        System.out.println(ans);
+    }
+
+    /**
+     * 输入：s = "rabbbit", t = "rabbit"
+     * 输出：3
+     */
+    static class Solution {
+        public int numDistinct(String s, String t) {
+
+            int ans = 0;
+
+
+            return ans;
+        }
+
+    }
+}
