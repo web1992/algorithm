431ecbab58d77765c87aa66acd5fb6e60c46a9ce
web1992 <554933654@qq.com>
2022-08-22 13:37:06 +0800

    add [不同的二叉搜索树.java]

--- src/main/java/cn/web1992/algorithm/leetcode/不同的二叉搜索树.java
+++ src/main/java/cn/web1992/algorithm/leetcode/不同的二叉搜索树.java
@@ -4,6 +4,7 @@ package cn.web1992.algorithm.leetcode;
  * @author web1992
  * @date 2022/7/4  22:17
  * @link {https://leetcode.cn/problems/unique-binary-search-trees/}
+ * @link {https://leetcode.cn/problems/unique-binary-search-trees/solution/shou-hua-tu-jie-san-chong-xie-fa-dp-di-gui-ji-yi-h/}
  * @link {https://programmercarl.com/0096.%E4%B8%8D%E5%90%8C%E7%9A%84%E4%BA%8C%E5%8F%89%E6%90%9C%E7%B4%A2%E6%A0%91.html}
  */
 public class 不同的二叉搜索树 {
@@ -18,7 +19,18 @@ public class 不同的二叉搜索树 {
 
         public int numTrees(int m) {
 
-            return 0;
+            int[] dp = new int[m + 1];
+
+            dp[0] = 1;// 空树
+            dp[1] = 1;
+
+            for (int i = 2; i <= m; i++) {
+                for (int j = 0; j <= i - 1; j++) {
+                    dp[i] += dp[j] * dp[i - j - 1];
+                }
+            }
+
+            return dp[m];
         }
     }
 }
