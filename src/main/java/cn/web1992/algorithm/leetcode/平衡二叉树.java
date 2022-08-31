229e57886149c37f0846babe313122449e947760
web1992 <554933654@qq.com>
2022-08-31 15:16:55 +0800

    add [平衡二叉树.java]

---
+++ src/main/java/cn/web1992/algorithm/leetcode/平衡二叉树.java
@@ -0,0 +1,44 @@
+package cn.web1992.algorithm.leetcode;
+
+/**
+ * @link {https://leetcode.cn/problems/balanced-binary-tree/}
+ * @link {https://programmercarl.com/0110.%E5%B9%B3%E8%A1%A1%E4%BA%8C%E5%8F%89%E6%A0%91.html}
+ */
+public class 平衡二叉树 {
+
+    class Solution {
+
+        public boolean isBalanced(TreeNode root) {
+            if (root == null) {
+                return true;
+            }
+            return getHeight(root) != -1;
+        }
+
+        public int getHeight(TreeNode node) {
+
+            if (null == node) {
+                return 0;
+            }
+
+            int leftH = getHeight(node.left);
+            if (leftH == -1) {
+                return -1;
+            }
+
+            int rightH = getHeight(node.right);
+            if (rightH == -1) {
+                return -1;
+            }
+
+            if (Math.abs(rightH - leftH) > 1) {
+                return -1;
+            } else {
+                // 最大的高度+1
+                return 1 + Math.max(rightH, leftH);
+            }
+        }
+    }
+
+
+}
