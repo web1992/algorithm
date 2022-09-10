c154adea18dec1ae94d30c54a9c8cfcffdac6b71
web1992 <554933654@qq.com>
2022-09-10 23:39:04 +0800

    add [分割等和子集.java]

--- src/main/java/cn/web1992/algorithm/leetcode/test/分割等和子集.java
+++ src/main/java/cn/web1992/algorithm/leetcode/test/分割等和子集.java
@@ -19,7 +19,25 @@ public class 分割等和子集 {
     static class Solution {
         public boolean canPartition(int[] nums) {
 
-            return false;
+            int sum = 0;
+            for (int num : nums) {
+                sum += num;
+            }
+
+            if (sum % 2 == 1) {
+                return false;
+            }
+
+            int target = sum / 2;
+            int[] dp = new int[10001];
+
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = target; j >= nums[i]; j--) {
+                    dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
+                }
+            }
+
+            return dp[target] == target;
         }
     }
 }
