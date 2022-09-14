d092bd29658ff0b08128b3ad45f8d9d75c134e62
web1992 <554933654@qq.com>
2022-09-14 11:39:06 +0800

    fix [排序]

--- src/main/java/cn/web1992/algorithm/sort/基数排序.java
+++ src/main/java/cn/web1992/algorithm/sort/基数排序.java
@@ -3,8 +3,10 @@ package cn.web1992.algorithm.sort;
 /**
  * @author web1992
  * @date 2022/4/18  10:21
- *  @link {https://time.geekbang.org/column/article/42038?screen=full}
+ * @link {https://github.com/wangzheng0822/algo/blob/master/java/13_sorts/RadixSort.java}
+ * @link {https://time.geekbang.org/column/article/42038?screen=full}
  * @implNote 基数排序（Radix sort）
+ * @implSpec  比如按照 手机号码 进行排序
  */
 public class 基数排序 {
     public static void main(String[] args) {
--- src/main/java/cn/web1992/algorithm/sort/桶排序.java
+++ src/main/java/cn/web1992/algorithm/sort/桶排序.java
@@ -4,8 +4,10 @@ package cn.web1992.algorithm.sort;
  * @author web1992
  * @date 2022/4/18  10:20
  * @link {https://time.geekbang.org/column/article/42038?screen=full}
+ * @link {https://github.com/wangzheng0822/algo/blob/master/java/13_sorts/BucketSort.java}
  * @implNote 线性排序（Linear sort）
  * @implNote 桶排序（Bucket sort）
+ * @implSpec 比如按照 金额 给数据排序
  */
 public class 桶排序 {
     public static void main(String[] args) {
