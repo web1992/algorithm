789aab6fd977f6b0c78b19250a8638e8d836a8bb
web1992 <554933654@qq.com>
2022-10-14 11:35:24 +0800

    add [环形链表.java]

--- src/main/java/cn/web1992/algorithm/leetcode/链表/环形链表.java
+++ src/main/java/cn/web1992/algorithm/leetcode/链表/环形链表.java
@@ -25,7 +25,19 @@ public class 环形链表 {
      * }
      */
     static public class Solution {
+
         public boolean hasCycle(ListNode head) {
+
+            ListNode fast = head, slow = head;
+
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (fast == slow) {
+                    return true;
+                }
+
+            }
 
             return false;
         }
