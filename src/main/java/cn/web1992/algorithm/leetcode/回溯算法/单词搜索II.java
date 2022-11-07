72652c0f5e7f3f5053d793866077d71d6a1d9af6
web1992 <554933654@qq.com>
2022-11-07 17:39:32 +0800

    add [单词搜索II.java]

--- src/main/java/cn/web1992/algorithm/leetcode/回溯算法/单词搜索II.java
+++ src/main/java/cn/web1992/algorithm/leetcode/回溯算法/单词搜索II.java
@@ -1,20 +1,39 @@
 package cn.web1992.algorithm.leetcode.回溯算法;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 /**
- * https://leetcode.cn/leetbook/read/top-interview-questions/xaorig/
+ * @link {https://leetcode.cn/leetbook/read/top-interview-questions/xaorig/}
+ * @link {https://leetcode.cn/problems/word-search-ii/solutions/1000172/dan-ci-sou-suo-ii-by-leetcode-solution-7494/}
  */
 public class 单词搜索II {
 
     public static void main(String[] args) {
 
+        //
+        char[][] board = new char[][]{{'a', 'b'}, {'c', 'd'}};
+        String[] words = new String[]{"abcb"};
+
+        List<String> ans = new Solution().findWords(board, words);
+        System.out.println(ans);
     }
 
-    class Solution {
-        public List<String> findWords(char[][] board, String[] words) {
+    static class Solution {
+        List<String> ans = new ArrayList<>();
 
-            return null;
+        public List<String> findWords(char[][] board, String[] words) {
+            do_findWords(board, new HashSet<>(Arrays.asList(words)), 0, 0);
+            return ans;
+        }
+
+        public void do_findWords(char[][] board, Set<String> set, int i, int j) {
+
+
         }
     }
 }
