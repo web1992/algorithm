e488ddba45291dd38ac958019d6bc705f48ec1d9
web1992 <554933654@qq.com>
2022-11-07 17:59:45 +0800

    add [单词搜索II.java]

--- src/main/java/cn/web1992/algorithm/leetcode/回溯算法/单词搜索II.java
+++ src/main/java/cn/web1992/algorithm/leetcode/回溯算法/单词搜索II.java
@@ -2,10 +2,12 @@ package cn.web1992.algorithm.leetcode.回溯算法;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Stream;
 
 /**
  * @link {https://leetcode.cn/leetbook/read/top-interview-questions/xaorig/}
@@ -24,16 +26,74 @@ public class 单词搜索II {
     }
 
     static class Solution {
-        List<String> ans = new ArrayList<>();
+
+        // 上，下，左，右 四个方向
+        int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
         public List<String> findWords(char[][] board, String[] words) {
-            do_findWords(board, new HashSet<>(Arrays.asList(words)), 0, 0);
-            return ans;
+            Trie trie = new Trie();
+            for (String word : words) {
+                trie.insert(word);
+            }
+
+            Set<String> ans = new HashSet<>();
+
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[i].length; j++) {
+                    do_findWords(board, ans, trie, i, j);
+                }
+            }
+
+            return new ArrayList<>(ans);
         }
 
-        public void do_findWords(char[][] board, Set<String> set, int i, int j) {
+        public void do_findWords(char[][] board, Set<String> set, Trie root, int i, int j) {
+            char ch = board[i][j];
 
+            if (!root.map.containsKey(ch)) {
+                return;
+            }
 
+            if (root.isWord()) {
+                set.add(root.word);
+            }
+
+            // 设置已经处理过的标记
+            board[i][j] = 'x';
+
+            for (int[] dir : dirs) {
+                int ii = i + dir[0], jj = j + dir[1];
+                if (ii >= 0 && ii < board.length && jj >= 0 && j < board[ii].length) {
+                    do_findWords(board, set, root, i, j);
+                }
+            }
+
+            // 还原
+            board[i][j] = ch;
+        }
+    }
+
+    static class Trie {
+
+        String word;
+        Map<Character, Trie> map = new HashMap<>();
+
+        public void insert(String word) {
+
+            Trie node = this;
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                if (!node.map.containsKey(ch)) {
+                    node.map.put(ch, new Trie());
+                }
+                node = node.map.get(ch);
+            }
+            node.word = word;
+
+        }
+
+        public boolean isWord() {
+            return null != word;
         }
     }
 }
