4f79d86c4cc9c1d32d8d9184b928e81514a77b08
web1992 <554933654@qq.com>
2022-11-22 11:47:13 +0800

    add 数据流的中位数

--- src/main/java/cn/web1992/algorithm/leetcode/堆栈/数据流的中位数.java
+++ src/main/java/cn/web1992/algorithm/leetcode/堆栈/数据流的中位数.java
@@ -7,32 +7,81 @@ import java.util.PriorityQueue;
  */
 public class 数据流的中位数 {
 
-    class MedianFinder {
+    public static void main(String[] args) {
 
-        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        // PriorityQueue<Integer> minHeap = new PriorityQueue<>(MedianFinder::cmp);
+        //
+        // minHeap.offer(1);
+        // minHeap.offer(9);
+        // minHeap.offer(6);
+        // while (!minHeap.isEmpty()) {
+        // System.out.println(minHeap.poll());
+        // }
+
+        MedianFinder medianFinder = new MedianFinder();
+        medianFinder.addNum(-1);
+        medianFinder.addNum(-2);
+        medianFinder.addNum(-3);
+        // medianFinder.addNum(4);
+        // medianFinder.addNum(5);
+        // medianFinder.addNum(3);
+        // medianFinder.addNum(6);
+        double median = medianFinder.findMedian();
+        System.out.println(median);
+    }
+
+    static class MedianFinder {
+
+        // 5-4-3-2-1
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(MedianFinder::cmp);// 放的数据都小于 minHeap 中的数据
+        // 50-60-70-80
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
         public MedianFinder() {
 
         }
 
         public void addNum(int num) {
-            queue.add(num);
+
+            if (maxHeap.isEmpty() && minHeap.isEmpty()) {
+                maxHeap.offer(num);
+                return;
+            } else if (maxHeap.isEmpty()) {
+                minHeap.offer(num);
+                maxHeap.offer(minHeap.poll());
+                return;
+            } else if (minHeap.isEmpty()) {
+                maxHeap.offer(num);
+                minHeap.offer(maxHeap.poll());
+                return;
+            }
+
+            if (num > minHeap.peek()) {
+                minHeap.offer(num);
+                if (minHeap.size() > maxHeap.size() + 1) {
+                    maxHeap.offer(minHeap.poll());
+                }
+            } else {
+                maxHeap.offer(num);
+                if (maxHeap.size() > minHeap.size() + 1) {
+                    minHeap.offer(maxHeap.poll());
+                }
+            }
+
         }
 
         public double findMedian() {
 
-            // 1-2-3-4
-            // 1-2-3-4-5
-            int len = queue.size();
-            Integer[] arr = queue.toArray(new Integer[queue.size()]);
-            if (len % 2 == 0) {
-                int i = (len / 2);
-                return (arr[i] + arr[i + 1]) / 2;
+            if (maxHeap.size() == minHeap.size()) {
+                return (minHeap.peek() + maxHeap.peek()) / 2.0;
             } else {
-                int i = (len / 2);
-                return arr[i + 1];
+                return minHeap.size() > maxHeap.size() ? minHeap.peek() : maxHeap.peek();
             }
 
+        }
+
+        static private int cmp(int n1, int n2) {
+            return n2 - n1;
         }
     }
 
