5598ad66ce7676445d332c851cd42e4b215c258f
web1992 <554933654@qq.com>
2022-12-05 17:59:39 +0800

    add [有序矩阵中第K小的元素.java]

---
+++ src/main/java/cn/web1992/algorithm/leetcode/堆栈/有序矩阵中第K小的元素.java
@@ -0,0 +1,18 @@
+package cn.web1992.algorithm.leetcode.堆栈;
+
+/**
+ * @link {https://leetcode.cn/leetbook/read/top-interview-questions/xaicbc/}
+ * @link {https://leetcode.cn/problems/kth-smallest-element-in-a-sorted-matrix/solutions/311472/you-xu-ju-zhen-zhong-di-kxiao-de-yuan-su-by-leetco/}
+ */
+public class 有序矩阵中第K小的元素 {
+
+
+    static class Solution {
+
+        public int kthSmallest(int[][] matrix, int k) {
+
+            return -1;
+        }
+
+    }
+}
