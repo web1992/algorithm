3e4d363e87fb18aa70ec85b98885716807ec07cd
web1992 <554933654@qq.com>
2022-12-06 13:57:02 +0800

    add [有序矩阵中第K小的元素.java]

--- src/main/java/cn/web1992/algorithm/leetcode/堆栈/有序矩阵中第K小的元素.java
+++ src/main/java/cn/web1992/algorithm/leetcode/堆栈/有序矩阵中第K小的元素.java
@@ -11,7 +11,44 @@ public class 有序矩阵中第K小的元素 {
 
         public int kthSmallest(int[][] matrix, int k) {
 
-            return -1;
+            int len = matrix.length;
+
+            int left = matrix[0][0];
+            int right = matrix[len - 1][len - 1];
+
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if (check(matrix, mid, k, len)) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+
+            }
+
+            return left;
+        }
+
+        public boolean check(int[][] matrix, int mid, int k, int n) {
+
+            int i = n - 1;
+            int j = 0;
+            int num = 0;
+
+            while (i >= 0 && j < n) {
+
+                // matrix[i][j] = 从左下角开始
+                if (matrix[i][j] <= mid) {
+                    num += i + 1;
+                    j++;// 矩阵中向右走
+                } else {
+                    i--;// 矩阵中向上走
+                }
+            }
+            // ..k...mid.......
+            // ......mid...k....
+            // 小于mid 的数的个数 大于 k，说明 k 在mid 的左边
+            return num >= k;
         }
 
     }
